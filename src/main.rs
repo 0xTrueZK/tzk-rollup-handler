d634899a10ff19bd55a702d103b5a78b7b2f8493
Agnish Ghosh <iamagnix@gmail.com>
2024-06-27 13:48:33 +0530

    add fixed code

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
 use aws_config::meta::region::RegionProviderChain;
-use aws_sdk_ec2::{config::Region, meta::PKG_VERSION, types::Tag, Client, Error};
+use aws_sdk_ec2::{Client, types::Tag};
 use serde::Deserialize;
 use warp::{http::StatusCode, Filter, Rejection, Reply, cors};
 
 #[derive(Debug, Deserialize)]
 struct InstanceRequest {
     ami_id: String,
+    instance_name: String,  // Field for the instance name
 }
 
-async fn create_instance(ami_id: String) -> Result<impl Reply, Rejection> {
+async fn create_instance(ami_id: String, instance_name: String) -> Result<impl Reply, Rejection> {
     let region_provider = RegionProviderChain::default_provider();
     let config = aws_config::from_env().region(region_provider).load().await;
     let client = Client::new(&config);
 
     let result = client.run_instances()
         .image_id(ami_id)
-        .instance_type(aws_sdk_ec2::types::InstanceType::T4gXlarge)
+        .instance_type(aws_sdk_ec2::types::InstanceType::T4gLarge)
         .min_count(1)
         .max_count(1)
+        .key_name("true-net-agnish")
+        .tag_specifications(aws_sdk_ec2::types::TagSpecification::builder()
+            .resource_type(aws_sdk_ec2::types::ResourceType::Instance)
+            .tags(Tag::builder().key("Name").value(instance_name).build())
+            .build())
         .send()
         .await;
 
@@ -28,34 +34,29 @@ async fn create_instance(ami_id: String) -> Result<impl Reply, Rejection> {
             Err(warp::reject::reject())
         },
     }
-
 }
 
 async fn create_instance_handler(body: InstanceRequest) -> Result<impl Reply, Rejection> {
-    create_instance(body.ami_id).await
+    create_instance(body.ami_id, body.instance_name).await
 }
 
 fn cors_filter() -> cors::Cors {
     warp::cors()
-        .allow_any_origin()
-        .allow_headers(vec![
-            "User-Agent",
-            "Content-Type",
-        ])
+        .allow_origins(vec!["http://localhost:3010", "http://staging-app.truezk.com", "http://app.truezk.com" , "https://staging-app.truezk.com", "https://app.truezk.com"])
+        .allow_headers(vec!["User-Agent", "Content-Type"])
         .allow_methods(vec!["POST"])
         .build()
 }
 
 #[tokio::main]
 async fn main() {
-
     let instance_route = warp::post()
         .and(warp::path("create_instance"))
         .and(warp::body::json())
         .and_then(create_instance_handler)
-        .with(cors_filter()); // Apply CORS settings here
+        .with(cors_filter());
 
     warp::serve(instance_route)
-        .run(([127, 0, 0, 1], 3030))
+        .run(([0, 0, 0, 0], 8080))
         .await;
 }
